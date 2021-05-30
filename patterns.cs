9837d4b6f4765187965b379f5e86b4684bb29a7c
Helenanas <83287055+Helenanas@users.noreply.github.com>
2021-05-30 22:34:23 +0300

    Add files via upload

---
+++ patterns.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.State.Conceptual
+{
+    class Admin
+    {
+        private Cake _cake = null;
+
+        public Admin(Cake cake)
+        {
+            this.TransitionTo(cake);
+        }
+
+    }
+
+    abstract class Cake
+    {
+        protected Vid_cake;
+
+        public void SetVid(Vid cake)
+        {
+            this._vid = cake;
+        }
+
+        public abstract void Name();
+
+    }
+
+    class Vid : Cake
+    {
+       
+
+        public override void Name()
+        {
+            Console.Cake("Vid");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код.
+            var vid = new Admin(new Vid());
+            vid.Request();
+        }
+    }
+}
