9faa2457a2add777bc982953faeb2d84c6c60ca5
Helenanas <83287055+Helenanas@users.noreply.github.com>
2021-05-30 22:42:19 +0300

    Add files via upload

---
+++ user.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Cakes
+{
+     class Admin
+     {
+        public Admin()
+        {
+           Admin = new HashSet<Admin>();
+           Vid = new HashSet<Vid>();
+        }
+
+        public int Id { get; set; }
+        public string Parol{ get; set; }
+        public long PaymentAccount { get; set; }
+        public string DirectPassvord { get; set; }
+
+        public virtual ICollection<Admin> Admin { get; set; }
+        public virtual ICollection<Vid> Vid { get; set; }
+     }
+
+    public partial class Cakes
+    {
+        public Cakes()
+        {
+            Cakes = new HashSet<Cakes>();
+        }
+
+        public int Id { get; set; }
+        public string Cakes { get; set; }
+        public virtual Cakes Cakes { get; set; }
+        public virtual ICollect<Cakes> Cakes { get; set; }
+    }
+
+
+
+
+
+
+
+}
